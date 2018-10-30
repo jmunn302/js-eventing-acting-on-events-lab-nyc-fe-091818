4f08445ccf3ec9d08356d66ec582e3862939a18b
Julie Munn <jmunn302@gmail.com>
2018-10-30 23:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const dodger = document.getElementById('dodger')
 
 dodger.style.backgroundColor = 'pink'
+
+dodger.style.left = '180px'
+
+let dodgerPosition = 0
+
+function moveDodger() {
+  
+}
+
+dodger.addEventListener('keydown', moveDodger)
