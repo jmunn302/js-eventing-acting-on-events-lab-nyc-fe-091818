65265b3e6fff647f96d7aba747c50833f9085b30
Julie Munn <jmunn302@gmail.com>
2018-10-31 00:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function moveDodger(event) {
   const keycode = event.which
   if (keycode === 37) { 
     let oldLeft = dodger.style.left
-    dodger.style.left = oldLeft + '10px'
+    dodger.style.left = oldLeft - '10px'
     
   } else if (keycode === 39) { 
     //right arrow
